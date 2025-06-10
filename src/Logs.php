1816e43555cf7c09a81ca10fad859ee2842f162f
Ariell Cannal <ariell@cannal.com.br>
2025-06-10 18:02:55 -0300

    Signed-off-by: Ariell Cannal <ariell@cannal.com.br>

--- src/Logs.php
+++ src/Logs.php
@@ -3,77 +3,80 @@ namespace CANNALLogs;
 
 class Logs
 {
-
-    private string $baseDir = APPPATH . '/logs/';
-
+    
+    private string $baseDir = APPPATH . 'logs' . DIRECTORY_SEPARATOR;
+    
     private string $logDir = '';
-
+    
     private string $logName = '';
-
+    
     private string $logExt = 'log';
-
+    
     public function __construct(string $logDir = null, string $logName = null)
     {
+        $logDir = $this->sanitizeDir($logDir);
+        $logName = $this->sanitizeName($logName, true);
         if ($logDir && $this->checkLogDir($logDir)) {
-            $this->logDir = $logDir;
+            $this->logDir = $logDir . DIRECTORY_SEPARATOR;
         }
-        if ($logName) {
-            if ($this->checkLogName($logName)) {
-                $this->logName = $logName;
-            } else {
-                $this->logName = date('Y-m-d_H:i:s') . '.' . $this->logExt;
-            }
+        if ($logName && $this->checkLogName($logName)) {
+            $this->logName = $logName;
+        } else {
+            $this->logName = date('Y.m.d') . '.' . $this->logExt;
         }
     }
-
+    
     public function getBaseDir(): string
     {
         return $this->baseDir;
     }
-
+    
     public function setBaseDir(string $baseDir): self
     {
+        $baseDir = $this->sanitizeDir($baseDir);
         $this->baseDir = $baseDir;
         return $this;
     }
-
+    
     public function getLogDir(): string
     {
         return $this->logDir;
     }
-
+    
     public function setLogDir(string $logDir): self
     {
-        if ($this->checkLogDir($logDir)) {
-            $this->logDir = $logDir;
+        $logDir = $this->sanitizeDir($logDir);
+        if ($logDir = $this->checkLogDir($logDir)) {
+            $this->logDir = $logDir . DIRECTORY_SEPARATOR;
             return $this;
         }
         return false;
     }
-
+    
     public function getLogName(): string
     {
         return $this->logName;
     }
-
+    
     public function setLogName(string $logName): self
     {
+        $logName = $this->sanitizeName($logName, true);
         if ($this->checkLogName($logName)) {
             $this->logName = $logName;
             return $this;
         }
         return false;
     }
-
-    public function checkLogDir(string $path): bool
+    
+    private function checkLogDir(string $path): bool
     {
-        if (is_dir($this->baseDir . $path)) {
+        if (realpath($this->baseDir . $path)) {
             return true;
         } else {
-            $parts = explode('/', $path);
+            $parts = explode(DIRECTORY_SEPARATOR, $path);
             $current = $this->baseDir;
             foreach ($parts as $part) {
-                $current .= '/' . $part;
+                $current .= $part . DIRECTORY_SEPARATOR;
                 if (! empty($part) && ! is_dir($current)) {
                     if (! mkdir($current, 0755)) {
                         return false;
@@ -86,31 +89,67 @@ class Logs
         }
         return false;
     }
-
-    function checkLogName(string $filename): bool
+    
+    private function checkLogName(string $filename): bool
     {
-        $ext = '.' . $this->logExt;
-        if (substr($filename, sizeof($filename) - sizeof($ext)) != $ext) {
-            $filename .= $ext;
-        }
-        if (is_file($this->baseDir . $this->logDir . $filename)) {
+        if (realpath($this->baseDir . $this->logDir . $filename)) {
             return true;
         } else if (! $stream = fopen($this->baseDir . $this->logDir . $filename, 'a')) {
             return false;
         } else {
             fclose($stream);
         }
-
+        
         if (is_file($this->baseDir . $this->logDir . $filename)) {
             return true;
         }
-
+        
         return false;
     }
-
-    function write(string $level, string $message): bool
+    
+    private function sanitizeName(string $filename = null, bool $checkExtension = false): string|null
     {
-        if ($fp = @fopen($this->baseDir . $this->logDir . $this->logName . '.' . $this->logExt, "a")) {
+        if (! $filename) {
+            return null;
+        }
+        if ($checkExtension) {
+            $ext = '.' . $this->logExt;
+            if (substr($filename, strlen($filename) - strlen($ext)) != $ext) {
+                $filename .= $ext;
+            }
+        }
+        $file_ext = pathinfo($filename, PATHINFO_EXTENSION);
+        $file_name_str = pathinfo($filename, PATHINFO_FILENAME);
+        
+        // Replaces all spaces with hyphens.
+        $file_name_str = str_replace(' ', '_', $file_name_str);
+        // Removes special chars.
+        $file_name_str = preg_replace('/[^A-Za-z0-9\-\_]/', '_', $file_name_str);
+        // Replaces multiple hyphens with single one.
+        $file_name_str = preg_replace('/-+/', '_', $file_name_str);
+        
+        $clean_file_name = $file_name_str;
+        if ($file_ext) {
+            $clean_file_name .= '.' . $file_ext;
+        }
+        
+        return $clean_file_name;
+    }
+    
+    private function sanitizeDir(string $file_dir = null): string|null
+    {
+        if (! $file_dir) {
+            return null;
+        }
+        foreach (explode(DIRECTORY_SEPARATOR, $file_dir) as $part) {
+            $safe_dir[] = $this->sanitizeName($part);
+        }
+        return implode(DIRECTORY_SEPARATOR, $safe_dir);
+    }
+    
+    public function write(string $level, string $message): bool
+    {
+        if ($fp = @fopen($this->baseDir . $this->logDir . $this->logName, "a+")) {
             $message = date('Y-m-d H:i:s') . ' [' . strtoupper($level) . '] ' . $message . PHP_EOL;
             fwrite($fp, $message);
             fclose($fp);
